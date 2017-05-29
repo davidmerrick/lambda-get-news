56e28597f15e8c776ea9d0f352bff705bc827b57
David Merrick <david.merrick@jivesoftware.com>
2017-05-28 23:09:15 -0700

    Rewrote in old promise syntax

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@
 
 import axios from 'axios'
 
-exports.handler = async function index(event, context, callback) {
+exports.handler = function index(event, context, callback) {
     let newsSource = "cnn";
     let url = `https://newsapi.org/v1/articles?source=${newsSource}&sortBy=top&apiKey=${process.env.NEWS_API_KEY}`;
-    let response = await axios.get(url);
-    let articles = response.data.articles;
-    let filtered = articles.filter(item => item.url.indexOf("politics") !== -1);
-    callback(null, filtered);
+    axios.get(url)
+        .then(response => {
+            let articles = response.data.articles;
+            let filtered = articles.filter(item => item.url.indexOf("politics") !== -1);
+            callback(null, filtered);
+        })
+        .catch(err => {
+            callback(err);
+        })
 };
